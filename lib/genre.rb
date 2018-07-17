56d3d79c49662cba94f0c45a8df26f21ab0720fc
Madeline Stark <madeline.stark1@gmail.com>
2018-07-17 00:17:58 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,53 @@
+#require_relative '../lib/concerns/findable.rb'
+
+class Genre
+  extend Concerns::Findable
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    #save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    new_genre = Genre.new(name)
+    new_genre.save
+    #binding.pry
+    new_genre
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    unless @songs.include?(song)
+      @songs << song
+    end
+  end
+
+  def artists
+    @artists = []
+    @songs.each do |song|
+      @artists << song.artist
+    end
+    @artists.uniq
+  end
+
+
+end
