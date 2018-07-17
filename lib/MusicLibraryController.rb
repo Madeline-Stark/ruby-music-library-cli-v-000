f17a99f406d85ff9e150de8ae6dcc8e20413d179
Madeline Stark <madeline.stark1@gmail.com>
2018-07-17 00:43:23 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -16,9 +16,8 @@ attr_accessor :path
 
   def call
     user_input = ""
+    puts "Welcome to your music library!"
     while user_input != "exit"
-
-      puts "Welcome to your music library!"
 
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
